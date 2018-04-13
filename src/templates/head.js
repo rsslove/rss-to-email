8815f2ab8d31b0b07a4026a95be2e944f3e9d703
Karl Hughes <khughes.me@gmail.com>
2018-04-13 17:51:24 -0500

    Updating format

--- src/templates/head.js
+++ src/templates/head.js
@@ -6,7 +6,7 @@ const template = compile(`<mjml>
        <mj-all font-size="16px" color="dimgray" font-family="Helvetica" align="left" />
        <mj-body background-color="#ffffff"></mj-body>
        <mj-section padding="10px 10px 0"></mj-section>
-       <mj-text padding="5px 15px"></mj-text>
+       <mj-text padding="5px 15px" line-height="22px"></mj-text>
        <mj-divider padding="10px 15px" border-width="1px" border-style="dashed" border-color="lightgrey"></mj-divider>
      </mj-attributes>
      <mj-style inline="inline">
