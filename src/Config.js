9d1f099aa5fcee62551fa0353229687f2fe41f2a
Karl Hughes <khughes.me@gmail.com>
2018-04-14 08:01:51 -0500

    Updating config with stampit

--- src/Config.js
+++ src/Config.js
@@ -1,128 +1,65 @@
-const fe = require('file-exists');
-const fs = require('fs');
-const isJson = require('is-valid-json');
+const stampit = require('stampit');
 
 const DEFAULT_CONFIG_WARNING_MESSAGE = 'No configuration file or object included. Defaults will be used.';
-const DEFAULT_CONFIG = {
+
+const HeaderConfig = stampit({
+  props: {
+    banner: undefined,
+    link: undefined,
+    title: undefined,
+  },
+  init(options) {
+    this.banner = options.banner;
+    this.link = options.link;
+    this.title = options.title;
+  },
+});
+
+const FeedConfig = stampit({
+  props: {
+    description: undefined,
+    title: undefined,
+    url: undefined,
+  },
+  init(options) {
+    this.description = options.description;
+    this.title = options.title;
+    this.url = options.url;
+  },
+});
+
+const DEFAULT_CONFIG_OBJECT = {
   accentColor: 'red',
   filename: 'example',
-  header: {
-    banner: 'http://www.example.com/image.png',
-    link: 'http://www.example.com/',
-    title: 'Example Header',
-  },
+  header: HeaderConfig({
+    description: 'A short custom feed description',
+    title: 'A custom feed title',
+    url: 'http://www.feedforall.com/sample.xml',
+  }),
   greeting: 'Hey there,',
   intro: 'Thanks for opening the email! Here are some links I want you to check out:',
-  feeds: [
-    {
-      description: 'A short custom feed description',
-      title: 'A custom feed title',
-      url: 'http://www.feedforall.com/sample.xml',
-    },
-    {
-      description: 'A short custom feed description',
-      title: 'A custom feed title',
-      url: 'http://www.feedforall.com/sample.xml',
-    },
-  ],
+  feeds: [FeedConfig({
+    description: 'A short custom feed description',
+    title: 'A custom feed title',
+    url: 'http://www.feedforall.com/sample.xml',
+  })],
   outro: "Thanks for reading. We'll be back next week with more!",
   signature: 'John Smith, CMO at Example Co.',
 };
 
-/**
- * Determines whether an input is a filepath or not
- * @param {(string|Object)} input
- * @returns {boolean}
- */
-function isFilepathString(input) {
-  return typeof input === 'string' && fe.sync(input);
-}
-
-/**
- * Determines whether an input string is a JSON object or not
- * @param {(string|Object)} input
- * @returns {boolean}
- */
-function isJsonString(input) {
-  return typeof input === 'string' && isJson(input);
-}
-
-/**
- * Determines whether an input is a configuration object or not
- * @param {(string|Object)} input
- * @returns {boolean}
- */
-function isConfigObject(input) {
-  return typeof input === 'object';
-}
-
-/**
- * Merge two configuration objects
- * @param {Object} config1
- * @param {Object} config2
- * @returns {Object}
- */
-function merge(config1, config2) {
-  return Object.assign(config1, config2);
-}
-
-class Config {
-  /**
-   * Instantiates a new config object based on a string or object input
-   * @param  {(string|Object)} input
-   * @returns {Object}
-   */
-  constructor(input) {
-    let config;
-
-    if (isFilepathString(input)) {
-      config = Config.getFromFile(input);
-    } else if (isJsonString(input)) {
-      config = Config.getFromJson(input);
-    } else if (isConfigObject(input)) {
-      config = Config.getFromObject(input);
-    } else {
-      console.warn(DEFAULT_CONFIG_WARNING_MESSAGE);
-      config = Config.getDefault();
+const Config = stampit({
+  props: DEFAULT_CONFIG_OBJECT,
+  init(options) {
+    if (Object.keys(options).length === 0 && options.constructor === Object) {
+      throw DEFAULT_CONFIG_WARNING_MESSAGE;
     }
 
-    return config;
+    Object.keys(this).forEach((property) => {
+      this[property] = options[property] || DEFAULT_CONFIG_OBJECT[property];
+    });
+  },
+  methods: {
   }
-
-  /**
-   * Get the default config object
-   * @returns {Object}
-   */
-  static getDefault() {
-    return DEFAULT_CONFIG;
-  }
-
-  /**
-   * Generate a config object from a json string
-   * @param {string} input
-   * @returns {Object}
-   */
-  static getFromJson(input) {
-    return merge(Config.getDefault(), Config.getFromObject(JSON.parse(input)));
-  }
-
-  /**
-   * Generate a config object from a filepath
-   * @param {string} input
-   * @returns {Object}
-   */
-  static getFromFile(input) {
-    return merge(Config.getDefault(), JSON.parse(fs.readFileSync(input)));
-  }
-
-  /**
-   * Generate a config object from an object
-   * @param {Object} input
-   * @returns {Object}
-   */
-  static getFromObject(input) {
-    return merge(Config.getDefault(), input);
-  }
-}
+});
 
 module.exports = Config;
--- tests/unit/Config.test.js
+++ tests/unit/Config.test.js
@@ -1,30 +1,10 @@
 const Config = require('../../src/Config');
-const fs = require('fs');
 
 describe('Config', () => {
   let config;
-  let expectedConfig;
-  console.warn = jest.fn();
 
-  beforeEach(() => {
-    console.warn.mockClear();
-    expectedConfig = Config.getDefault();
-  });
-
-  test('creates config object, shows warning when no inputs given', () => {
-    config = new Config();
-
-    expect(console.warn).toHaveBeenCalled();
-    expect(config).toEqual(expectedConfig);
-  });
-
-  test('creates config object when filepath input given', () => {
-    const input = './fixtures/config.test.json';
-    const inputObject = JSON.parse(fs.readFileSync(input));
-
-    config = new Config(input);
-
-    expect(config).toEqual(Object.assign(expectedConfig, inputObject));
+  test('throws exception when no inputs given', () => {
+    expect(() => Config()).toThrow();
   });
 
   test('creates config object when object input given', () => {
@@ -38,14 +18,7 @@ describe('Config', () => {
 
     config = new Config(input);
 
-    expect(config).toEqual(Object.assign(expectedConfig, input));
-  });
-
-  test('creates config object using setConfigFromFile method', () => {
-    const input = './config.example.json';
-
-    config = Config.getFromFile(input);
-
-    expect(config).toEqual(expectedConfig);
+    expect(config.accentColor).toEqual(input.accentColor);
+    expect(config.header).toEqual(input.header);
   });
 });
