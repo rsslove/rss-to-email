760be79026874e8eb1409c6ef008414da92067c6
Karl Hughes <khughes.me@gmail.com>
2018-06-15 14:30:47 -0500

    Adding social preview, lots of good stuff

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -3,19 +3,33 @@ $('.modal-activator').click(function () {
   $('input#mce-FEATURE').val($(this).data('feature'));
 });
 
+// Fill in a sample
+$('#view-sample').click(function () {
+  $('form#email-options input[name="header[title]"]').val('Tech Crunch Newsletter');
+  $('form#email-options input[name="feeds[][url]"]').val('http://feeds.feedburner.com/TechCrunch/');
+  $('form#email-options input[name="accentColor"]').val('#00a562');
+  $('form#email-options input[name="header[banner]"]').val('https://i.imgur.com/Zpr0Gyf.png');
+  $('form#email-options input[name="header[link]"]').val('https://techcrunch.com/');
+  $('form#email-options').submit();
+
+  return false;
+});
+
 // Handle form submission
 var baseUrl = 'https://wt-972cd1bf439680cd8845b3d13ca8326e-0.sandbox.auth0-extend.com/rss-to-email-webtask';
 $('form#email-options').submit(function () {
+  $('#alert-dialog').hide();
   // Get the form data
   var formData = formToObject('email-options');
   formData.feeds = Object.values(formData.feeds);
-
-  // Get default data
+  // Merge it with default data
   var data = getDefaultData();
-
   $.extend(data, formData);
 
-  console.log(data);
+  // Show loading indicators
+  $('#preview-b1 h1').html('<i class="fa fa-spin fa-spinner"></i>');
+  $('#html-b1 h1').html('<i class="fa fa-spin fa-spinner"></i>');
+  $('#submit-options-form').prop('disabled', true);
 
   $.ajax(baseUrl, {
     data : JSON.stringify(data),
@@ -23,11 +37,17 @@ $('form#email-options').submit(function () {
     contentType : 'application/json',
     type : 'POST',
     success: function (response) {
-      console.log(response);
       $('#preview-b1').html(response);
       response = $('<div/>').text(response).html();
       $('#html-b1').html('<pre><code>' + response + '</code></pre>');
-    }
+    },
+    error: function () {
+      $('#alert-dialog').text('Something went wrong. Please refresh the page and try again.');
+      $('#alert-dialog').show();
+    },
+    complete: function () {
+      $('#submit-options-form').prop('disabled', false);
+    },
   });
 
   return false;
--- docs/readme.md
+++ docs/readme.md
@@ -1,6 +1,8 @@
 # RSS-to-Email Demo
- 
-## Dev Notes
+
+This demo site illustrates using RSS-To-Email with a backend API and frontend preview. It is built on Jekyll, and you can run or build or run it locally with Docker:
 
 - Build the site: `docker run --rm -it -v $PWD:/srv/jekyll jekyll/jekyll:latest jekyll build`
 - Serve locally: `docker run --rm -it -v $PWD:/srv/jekyll -p 4000:4000 jekyll/jekyll:latest jekyll serve`
+
+To deploy, I recommend using Github pages.
